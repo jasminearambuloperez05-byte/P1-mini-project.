0f2ecde8172e3ae6fe9e3dd3ec93705e12d87fb2
jasminearambuloperez05-byte <jasminearambuloperez05@gmail.com>
2026-08-17 23:30:30 +0800

    main.py
    Corrected syntax errors and improved user prompts for input. Added comments for better understanding.

---
+++ main.py
@@ -0,0 +1,21 @@
+num1 = float(input("Enter the first number.")
+num2 = float(input("Enter the second number.")
+num3 = float(input("Enter the third number.") 
+total - num1 + num2 + num3
+print(f "The sum of the three numbers is: (total)
+UNDERSTANDING & TO EXPLAIN TO INSTRUCTOR)
+input(): Prompts the user & reads what they type. int() converts text input into a whole number.
+Variables: num1, num2, num3 store the 3 values; total stores their sum.
+Print(): Outputs the final result in the required format.
+Enter the first number: 5 
+Enter the second number: 10
+Enter the third number: 15
+The sum of the three numbers: 30
+GET THE THREE NUMBERS FROM THE USER, ONE AT A TIME
+num1 = int(input("Enter the first number."))
+num2 = int(input("Enter the second number."))
+num3 = int(input("Enter the third number."))
+CALCULATE THE SUM
+total = num1 + num2 + num3
+DISPLAY THE REULT
+print(f" The sum of the three number is (total)
